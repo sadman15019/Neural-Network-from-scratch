ac40ac6da0737fd8cb82b6c13637753022916bbb
sadman 15019 <sakib1807020@stud.kuet.ac.bd>
2023-03-10 01:10:34 +0600

    first push

---
+++ hidden layer.py
@@ -0,0 +1,18 @@
+import numpy as np
+X=[[1,2,3,2.5],
+   [2.0,5.0,-1.0,2.0],
+   [-1.5,2.7,3.3,-0.8]]
+
+class Dense_layer:
+    def __init__(self,n_inputs,n_neurons):
+       self.weights=np.random.randn(n_inputs,n_neurons)
+       self.biases=np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output=np.dot(inputs,self.weights)+self.biases
+        
+layer1=Dense_layer(4,5)
+layer2=Dense_layer(5,2)
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer1.output)
+print(layer2.output)
